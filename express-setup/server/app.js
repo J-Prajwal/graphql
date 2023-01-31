8ba4efd20ce56632424f2ba6671b9dfb8127d5c7
J-Prajwal <luckyjais058@gmail.com>
2023-01-31 23:57:17 +0530

    express-app setup with graphql

---
+++ express-setup/server/app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const { graphqlHTTP } = require("express-graphql");
+const schema = require("./schema/schema");
+
+const app = express();
+
+// express has no idea about graphql so whenever any request on /graphql is made graphqlHTTP() is fired as middleware
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: schema,
+  })
+);
+
+app.listen(4000, () => {
+  console.log("Your app is listening on http://localhost:4000/");
+});
